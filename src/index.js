47f1814218dac3e1190e0cc095d825e1c24ff5e9
J-724 <jjpernian@gmail.com>
2022-05-19 22:43:07 -0400

    Split entries

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 import _ from 'lodash';
-import myName from './myName';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['hello', 'webpack'], '' ) +' '+ myName('Ryuk');
-    
+    element.innerHTML = _.join(['hello', 'webpack'], '' );
+  
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;   
 }
 document.body.appendChild(component());
-
-
-
-// TOP extra tutorial
-
-function componentTOP() {
-    const element = document.createElement('div');
-    // use your function!
-    element.textContent = myName('Cody');
-    return element;
-  }
-document.body.appendChild(componentTOP());
